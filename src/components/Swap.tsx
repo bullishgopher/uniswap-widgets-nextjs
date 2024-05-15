84aff5bd0a8ab2eb63b58ce99b1cc1b1d8298081
bullishgopher <49041460+bullishgopher@users.noreply.github.com>
2024-05-15 03:35:09 -0700

    feat: integrate uniswap widgets

--- src/components/Swap.tsx
+++ src/components/Swap.tsx
@@ -1,18 +1,27 @@
-import { useEthersProvider } from "@/provider";
-import { JsonRpcProvider, SwapWidget } from "@uniswap/widgets";
-import "@uniswap/widgets/fonts.css";
+import "@dex-swap/widgets/fonts.css";
+import dynamic from 'next/dynamic';
+
+const SwapWidget = dynamic(() => import('@dex-swap/widgets').then(pkg => pkg.SwapWidget), {
+  ssr: false,
+});
+
+const JSON_RPC_URL = 'https://cloudflare-eth.com'
+// The url of the default uniswap token list. This list will be passed to the Uniswap component
+// and will appear by default in the token selector UI.
+const TOKEN_LIST = 'https://ipfs.io/ipns/tokens.uniswap.org'
 
 export const Swap = () => {
-  const provider = useEthersProvider();
+
+
   return (
     <div>
       <SwapWidget
-        provider={provider as JsonRpcProvider}
         jsonRpcUrlMap={{
           1: [
-            "https://eth-sepolia.g.alchemy.com/v2/Z1TiAbk4rG-ZI73AYpuXYNTTuBVOiMEU",
+            JSON_RPC_URL,
           ],
         }}
+        tokenList={TOKEN_LIST}
         defaultChainId={1}
       />
     </div>
